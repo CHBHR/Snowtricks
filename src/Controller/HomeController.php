cd316eefef7d3a1fd35257abb1059ed70e236cef
CHBHR <chrishb.rey@gmail.com>
2022-11-11 14:50:53 +0100

    modification des pages twigs pour prendre en compte les fixtures de figure dans la db et affichage des contenus

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -2,24 +2,31 @@
 
 namespace App\Controller;
 
+use App\Entity\Figure;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use App\Repository\FigureRepository;
 
 class HomeController extends AbstractController
 {
     #[Route('/', name: 'app_home')]
-    public function index(): Response
+    public function index (FigureRepository $repo): Response
     {
+        $figures = $repo->findAll();
+
         return $this->render('website/index.html.twig', [
             'controller_name' => 'HomeController',
+            'figures' => $figures,
         ]);
     }
 
-    #[Route('/figure/12', name: 'app_show')]
-    public function show()
+    #[Route('/figure/{id}', name: 'app_show')]
+    public function show(Figure $figure)
     {
-        return $this->render('website/show.html.twig');
+        return $this->render('website/show.html.twig', [
+            'figure' => $figure,
+        ]);
     }
 
 }
