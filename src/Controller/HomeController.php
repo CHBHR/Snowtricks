a0ee32b517bdbbd9463bb41464ee85d2119f53ef
CHBHR <chrishb.rey@gmail.com>
2022-11-15 17:28:31 +0100

    ajout des formulaires pour la version simplifiée des figures

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -3,10 +3,14 @@
 namespace App\Controller;
 
 use App\Entity\Figure;
+use App\Form\FigureType;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use App\Repository\FigureRepository;
+use Doctrine\Persistence\ManagerRegistry;
+use Doctrine\Persistence\ObjectManager;
+use Symfony\Component\HttpFoundation\Request;
 
 class HomeController extends AbstractController
 {
@@ -21,7 +25,51 @@ class HomeController extends AbstractController
         ]);
     }
 
-    #[Route('/figure/{id}', name: 'app_show')]
+    /**
+     * L'ordre de déclaration des routes permet de ne pas avoir d'erreur lors de l'appel si plusieurs routes ont des noms similaires
+     */
+    #[Route('/figure/new', name: 'app_figure_create')]
+    #[Route('/figure/{id}/edit', name: 'app_figure_edit')]
+    public function formFigure(Figure $figure = null, Request $request, ManagerRegistry $doctrine )
+    {
+        $entityManager = $doctrine->getManager();
+
+        if(!$figure){
+            $figure = new Figure();
+        }
+
+        // $form = $this->createFormBuilder($figure)
+        //              ->add('nom')
+        //              ->add('description')
+        //              ->getForm();
+
+        $form = $this->createForm(FigureType::class, $figure);
+
+        $form->handleRequest($request);
+
+        if($form->isSubmitted() && $form->isValid()) {
+            if(!$figure->getId()){
+                $figure->setDateCreation(new \DateTime());
+                $figure->setDateModification(new \DateTime());
+            }
+            $figure->setDateModification(new \DateTime());
+
+            $entityManager->persist($figure);
+            $entityManager->flush();
+
+            return $this->redirectToRoute('app_figure_show', ['id' => $figure->getId()]);
+        }
+
+        /**
+         * $form étant un object complex, passe le résultat de la fonction createView() à twig pour qu'il puisse e traiter
+        */
+        return $this->render('website/create.html.twig', [
+            'formNewFigure' => $form->createView(),
+            'formEditFigure' => $figure->getId() !== null
+        ]);
+    }
+
+    #[Route('/figure/{id}', name: 'app_figure_show')]
     public function show(Figure $figure)
     {
         return $this->render('website/show.html.twig', [
